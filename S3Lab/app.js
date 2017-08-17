6ca2bd62e36ae95553e3c4759c742a773e83151d
Deep Chakraborty <deep9c@gmail.com>
2017-08-16 20:58:24 -0400

    Charts

--- S3Lab/app.js
+++ S3Lab/app.js
@@ -12,6 +12,11 @@ var auth = require('./auth.js')();
 var routes = require('./routes');
 var modelDBRoutes = require('./modelDBRoutes');
 
+
+var cpuStat = require('cpu-stat');
+var memStat = require('mem-stat');
+//var si = require('systeminformation');
+
 process.env.NODE_ENV = 'est';
 
 app.use(cors({origin: true,credentials: true}));
@@ -26,6 +31,74 @@ app.use('/',modelDBRoutes);
 var server = app.listen(8889,function() {
    console.log('App listening on port 8889.'); 
 });
+var io= require('socket.io')(server);
 
+//***** Socket.IO for Charts STARTS **********
+var i=0;
+var accTrn = [];
+var accVal = [];
+var lossTrn = [];
+var lossVal = [];
+var stubcounter = 0; //delete this line
+io.on('connection', function(socket) {
+	console.log('io.on connection');
+	++i;
+	socket.emit('news','hello world');
+	socket.on('my other event', function(data) {
+	console.log('my other event');
+	
+	//------------------STUB CODE STARTS -------------------
+	stubcounter++;
+	if(stubcounter>=10){
+		socket.disconnect(0);
+	}
+	var a = parseFloat(Math.random()).toFixed(2); 
+	    accTrn.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    accVal.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    lossTrn.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    lossVal.push(a);
 
+	    var usedPercent = Math.round(40);
+		var message = '{"kpi" : '+(90+a*10)+ ', "sec_ep" : '+(11.5+a*10)+ ', "batch" : '+ Math.round(a*380)+ ', "sam_s" : '+ a*2000 +', "CPU" : '+ Math.round(35) + ', "RAM": ' + usedPercent+', "acTr": ['+ accTrn+'], "acVal": ['+ accVal+'], "lsTr": ['+ lossTrn+'], "lsVal": ['+ lossVal+']}'; 
+		//console.log(message);
+	    socket.emit('news',  JSON.parse(message));
+
+	    //console.log(usedPercent);
+	    //the percentage cpu usage for core 0
+	    //console.log(Math.round(35));i
+	//---------------- STUB CODE ENDS ----------------------
+	
+	//-------UNCOMMENT the following later ---------------
+	/*cpuStat.usagePercent({
+	    sampleMs: 10000,
+	  },
+	  function(err, percent, seconds) {
+	    if (err) {
+	      return console.log(err);
+	    }
+	    var a = parseFloat(Math.random()).toFixed(2); 
+	    accTrn.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    accVal.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    lossTrn.push(a);
+	    a = parseFloat(Math.random()).toFixed(2);
+	    lossVal.push(a);
+
+	    var usedPercent = Math.round(memStat.usedPercent());
+		var message = '{"kpi" : '+(90+a*10)+ ', "sec_ep" : '+(11.5+a*10)+ ', "batch" : '+ Math.round(a*380)+ ', "sam_s" : '+ a*2000 +', "CPU" : '+ Math.round(percent) + ', "RAM": ' + usedPercent+', "acTr": ['+ accTrn+'], "acVal": ['+ accVal+'], "lsTr": ['+ lossTrn+'], "lsVal": ['+ lossVal+']}'; 
+		console.log(message);
+	    socket.emit('news',  JSON.parse(message));
+
+	    console.log(usedPercent);
+	    //the percentage cpu usage for core 0
+	    console.log(Math.round(percent));i
+
+	});*/
+	});
+}); 
+//******* Socket.IO for Charts ENDS *********
 
